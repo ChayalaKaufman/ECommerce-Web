a0ab5ee341cc507a46f227b3af2ad04c3e08f96f
Chayala Kaufman <chayalakaufman@gmail.com>
2019-04-14 13:51:36 -0400

    Add project files.

---
+++ homework 041019 ECommerce Website/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace homework_041019_ECommerce_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
